2d1db9e5dbac4e28f736f8f1ab7e82e8915d3f52
Daniel Páez <danielpaezsw66@gmail.com>
2020-03-03 21:24:19 -0300

    Stickers updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,83 @@
 const products = `
+0. Normal (sin producto)
 1. Mini Pan de Jamon
 2. Queso Crema jamon
 3. Queso Crema Salame
 4. Queso Crema Champiñon
 5. Cachito
 6. Golfeado
-7. Normal (sin producto)
 `;
 
-const defaultValue = 7
-const value = /*prompt(`¿Qué quieres imprimir? ${products}`) ||*/ defaultValue
+const article = {
+	products: [
+	 "Normal (sin producto)",
+	 "Mini Pan de Jamon",
+	 "Queso Crema jamon",
+	 "Queso Crema Salame",
+	 "Queso Crema Champiñon",
+	 "Cachito",
+	 "Golfeado"
+	]
+}
+
+
+const defaultValue = 0
+const stickerSelected = parseInt(prompt(`¿Qué quieres imprimir? ${products}`)) || defaultValue
+
+console.log(stickerSelected)
 const articles = document.getElementsByTagName("article");
 const articlesArray = [...articles]
 
-/*
-articlesArray.map((article) => {
-	article.style.backgroundImage = `url("./assets/img/${value}.png")`;
-})
-*/
+
+const main = document.getElementsByTagName("main")[0];
+
+
+// Son 88 elementos a insertar
+totalArticles = 88;
+
+const ArticleTemplate = (stickerSelected) => (
+ `<article class="item">
+		<img 
+			src="assets/img/transparente-logo.png" 
+			class="item__logo"
+		/>
+		<p class="item__slogan">Un placer para tu paladar</p>
+		<small class="item__product">
+			${article.products[stickerSelected]}
+		</small>
+	<section class="item__social-network">
+		<img 
+			src="assets/img/instagram.png" 
+			class="item__social-network__icon" 
+		/>
+		<p class="item__social-network__user">@donuts.paradise</p>
+	</section>
+</article>`
+)
+	
+
+const createTemplate = (HTMLString) => {
+		
+	const $html = document.implementation.createHTMLDocument();
+	$html.body.innerHTML = HTMLString;
+	return $html.body.children[0];
+}
+
+for (let i = 0; i < totalArticles; i++) {
+
+	const articleString = ArticleTemplate(stickerSelected);
+	const $article = createTemplate(articleString)
+	main.appendChild($article);
+}
+
+	if(stickerSelected === 0) {
+		const productTitles = document.querySelectorAll("body > main > article > small")
+		console.log(productTitles)
+		const productTitlesArray = [...productTitles]
+		productTitlesArray.map((title) => {
+			title.style.visibility = "hidden";
+		})
+	}
+
+
+
